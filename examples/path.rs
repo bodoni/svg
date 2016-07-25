50a1c853709d451a63c64db31f2ead74c991aa8b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-07-25 15:20:09 +0200

    Simplify the example

--- examples/path.rs
+++ examples/path.rs
@@ -7,39 +7,16 @@ use svg::tag::path::{Command, Data};
 fn main() {
     let path = "tests/fixtures/benton.svg";
     for event in svg::open(path).unwrap() {
-        match event {
-            Event::Tag(Tag::Path(_, attributes)) => {
-                let data = attributes.get("d").unwrap();
-                let data = Data::parse(data).unwrap();
-                draw(data);
-            },
-            _ => {
-                println!("Not sure how to react.");
-            },
-        }
-    }
-}
-
-fn draw(data: Data) {
-    for command in data.iter() {
-        match *command {
-            Command::MoveTo(_, ref parameters) => {
-                println!("Move to {:?}.", parameters);
-            },
-            Command::LineTo(_, ref parameters) => {
-                println!("Line to {:?}.", parameters);
-            },
-            Command::CurveTo(_, ref parameters) => {
-                println!("Curve to {:?}.", parameters);
-            },
-            Command::SmoothCurveTo(_, ref parameters) => {
-                println!("Smooth curve to {:?}.", parameters);
-            },
-            Command::ClosePath => {
-                println!("Close the path.");
-            },
-            _ => {
-                println!("Not sure what to do.");
+        if let Event::Tag(Tag::Path(_, attributes)) = event {
+            let data = attributes.get("d").unwrap();
+            let data = Data::parse(data).unwrap();
+            for command in data.iter() {
+                match command {
+                    &Command::MoveTo(..) => println!("Move!"),
+                    &Command::LineTo(..) => println!("Line!"),
+                    &Command::CurveTo(..) => println!("Curve!"),
+                    _ => {},
+                }
             }
         }
     }
