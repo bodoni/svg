6eb41e9d1aa0999bb1b7cf48683ec45149fe2609
Felix Zwettler <f.zwettler@posteo.de>
2024-02-16 14:51:28 +0100

    add blob node

---
+++ src/node/blob.rs
@@ -0,0 +1,55 @@
+use std::collections::hash_map::DefaultHasher;
+use std::fmt;
+use std::hash::Hash;
+
+use crate::node::{Node, Value};
+
+/// A blob node.
+#[derive(Clone, Debug)]
+pub struct Blob {
+    content: String,
+}
+
+impl Blob {
+    /// Create a node.
+    #[inline]
+    pub fn new<T>(content: T) -> Self
+    where
+        T: Into<String>,
+    {
+        Blob {
+            content: content.into(),
+        }
+    }
+}
+
+impl fmt::Display for Blob {
+    #[inline]
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        self.content.fmt(formatter)
+    }
+}
+
+impl Node for Blob {
+    #[inline]
+    fn append<T>(&mut self, _: T)
+    where
+        T: Into<Box<dyn Node>>,
+    {
+    }
+
+    #[inline]
+    fn assign<T, U>(&mut self, _: T, _: U)
+    where
+        T: Into<String>,
+        U: Into<Value>,
+    {
+    }
+}
+
+impl super::NodeDefaultHash for Blob {
+    #[inline]
+    fn default_hash(&self, state: &mut DefaultHasher) {
+        self.content.hash(state);
+    }
+}
